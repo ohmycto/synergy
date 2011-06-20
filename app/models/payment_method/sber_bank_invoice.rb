30fd37667ff8d2ada8c13dbeac5bf70674d96c6b
Alexander Shuhin <a.medium@gmail.com>
2011-06-20 17:10:51 +0400

    Changed ancestor class for sberbank_invoice

--- app/models/payment_method/sber_bank_invoice.rb
+++ app/models/payment_method/sber_bank_invoice.rb
@@ -1,4 +1,4 @@
-class PaymentMethod::SberBankInvoice < PaymentMethod
+class PaymentMethod::SberBankInvoice < PaymentMethod::Check
   preference :recipient, :string
   preference :inn, :string
   preference :kpp, :string
@@ -8,32 +8,6 @@ class PaymentMethod::SberBankInvoice < PaymentMethod
   preference :recipient_bank, :string
   preference :recipient_bank_city, :string
 
-  def actions
-    %w{capture void}
-  end
-
-  # Indicates whether its possible to capture the payment
-  def can_capture?(payment)
-    ['checkout', 'pending'].include?(payment.state)
-  end
-
-  # Indicates whether its possible to void the payment.
-  def can_void?(payment)
-    payment.state != 'void'
-  end
-
-  def capture(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.complete
-    true
-  end
-
-  def void(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.void
-    true
-  end
-  
   def desc
     "<p>#{I18n.t('sberbank_invoice')}</p>".html_safe
   end
