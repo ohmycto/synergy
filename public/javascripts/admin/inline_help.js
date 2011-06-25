dfd8260a73d4633b93ec0e2654887c82acb4fcad
Roman Smirnov <roman@railsdog.com>
2011-06-25 19:11:07 +0400

    Improved inline help to support formtastic-forms

--- public/javascripts/admin/inline_help.js
+++ public/javascripts/admin/inline_help.js
@@ -34,8 +34,11 @@ fields = {
 
 function register_help() {
   jQuery.each(fields, function(element_id, help_msg) {
-    jQuery('label[for='+element_id+']').after("&nbsp;<span class='help' id='help_"+element_id+"'><img src='/images/admin/icons/help.png' alt='help' /></span>");
-    jQuery('#help_'+element_id).simpletip({ position: 'right', offset: [-10, -230], content: help_msg});
+    if (jQuery('#help_'+element_id).length == 0)
+    {
+      jQuery('label[for='+element_id+']').append("&nbsp;<span class='help' id='help_"+element_id+"'><img src='/images/admin/icons/help.png' alt='help' /></span>");
+      jQuery('#help_'+element_id).simpletip({ position: 'right', offset: [-10, -230], content: help_msg});
+    }
   });
 }
 
